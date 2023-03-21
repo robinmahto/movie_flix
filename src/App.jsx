0e6f1dc24944eef7319fa82da5c9df6460c224c0
robinmahto <robinmahto413@gmail.com>
2023-03-21 22:37:34 +0530

    configure redux-toolkit in an app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
+import { useEffect } from 'react';
+import { fetchDataFromApi } from './utils/api';
+import { getApiConfiguration } from './store/homeSlice';
+import { useDispatch, useSelector } from 'react-redux';
 
 function App() {
+  const dispatch = useDispatch()
+  const {results} = useSelector((state)=> state.home.url)
+  const fetchData = async() => {
+   const result = await fetchDataFromApi('/movie/popular')
+   dispatch(getApiConfiguration(result))
+  }
+  useEffect(()=>{
+    fetchData();
+  }, [])
   return (
     <>
+    {
+      results?.map((data)=>{
+        return <div key={data.id}>
+          <p>{data.original_title}</p>
+          <p>{data.overview}</p>
+        </div>
+      })
+    }
     </>
   )
 }
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,15 +1,14 @@
 import axios from "axios";
 axios.defaults.baseURL = "https://api.themoviedb.org/3";
-
 const TMDB_TOKEN = import.meta.TMDB_TOKEN;
 
 const headers = {
     Authorization : `bearer ${TMDB_TOKEN}`
 }
 
-export const fetchDataFromApi = async(url, body)=> {
+export const fetchDataFromApi = async(url, params)=> {
       try {
-        const {data} = await axios.get(url, {headers, body});
+        const {data} = await axios.get(url, {headers, params});
         return data;
       } catch (error) {
         console.log(error);
